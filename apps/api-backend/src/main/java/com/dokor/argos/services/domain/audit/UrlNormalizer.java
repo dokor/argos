90af46f7768ab07cc484f70c6c55c7513ed8fe84
antoine-le-louet <a.lelouet.freelance@gmail.com>
2026-02-05 17:51:39 +0100

    Inject UrlNormalizer

--- apps/api-backend/src/main/java/com/dokor/argos/services/domain/audit/UrlNormalizer.java
+++ apps/api-backend/src/main/java/com/dokor/argos/services/domain/audit/UrlNormalizer.java
@@ -1,12 +1,18 @@
 package com.dokor.argos.services.domain.audit;
 
-
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import java.net.URI;
 
 @Singleton
 public class UrlNormalizer {
+
+    @Inject
+    public UrlNormalizer(
+    ) {
+        // TODO document why this constructor is empty
+    }
 
     public String normalize(String inputUrl) {
         URI uri = URI.create(inputUrl.trim());
