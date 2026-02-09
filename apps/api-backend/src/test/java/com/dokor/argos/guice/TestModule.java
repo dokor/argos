c9e18f33c5361d1b16ec7d0668bb58174e00d607
antoine-le-louet <a.lelouet.freelance@gmail.com>
2026-02-09 17:02:50 +0100

    add Test

--- apps/api-backend/src/test/java/com/dokor/argos/guice/TestModule.java
+++ apps/api-backend/src/test/java/com/dokor/argos/guice/TestModule.java
@@ -22,6 +22,6 @@ public class TestModule extends AbstractModule {
 			}
 		}));
         // To run database Flyway scripts before running tests
-         install(new GuiceDbTestModule());
+//         install(new GuiceDbTestModule()); //todo: a decommenter
 	}
 }
---
+++ apps/api-backend/src/test/java/com/dokor/argos/services/analysis/SampleIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.dokor.argos.services.analysis;
+
+import com.coreoz.test.GuiceTest;
+import com.dokor.argos.guice.TestModule;
+import com.dokor.argos.services.domain.audit.enums.AuditRunStatus;
+import com.dokor.argos.webservices.api.audits.AuditsWs;
+import com.dokor.argos.webservices.api.audits.data.CreateAuditRequest;
+import jakarta.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * An integration test sample.
+ * <p>
+ * This tests differs from an unit tests, cf {@link com.dokor.argos.SampleTest}, because:
+ * - It will initialize and rely on the dependency injection, see {@link TestModule} for tests specific overrides
+ * - Other services can be referenced for this tests
+ * - These other services can be altered for tests, see {@link com.coreoz.plume.mocks.MockedClock} for an example
+ * - If a database is used in the project, an H2 in memory database will be available to run queries and verify that data is correctly being inserted/updated in the database
+ * - The H2 in memory database will be created by playing Flyway initialization scripts: these scripts must be correctly setup
+ * <p>
+ * Integration tests are a great tool to test the whole chain of services with one automated test.
+ * Although, to intensively test a function, a unit test is preferred, see {@link com.dokor.argos.SampleTest} for an example.
+ * <p>
+ * Once there are other integration tests in the project, this sample should be deleted.
+ */
+@Slf4j
+@GuiceTest(TestModule.class)
+public class SampleIntegrationTest {
+    @Inject
+    JavaHttpUrlAuditAnalyzer javaHttpUrlAuditAnalyzer;
+
+    @Test
+    public void integration_test_scenario_description() {
+        String url = "https://lelouet.fr/";
+        UrlAuditResult result = javaHttpUrlAuditAnalyzer.analyze(url);
+        logger.info("Résultat de l'analyse de [{}] : {}", url, result);
+    }
+}
