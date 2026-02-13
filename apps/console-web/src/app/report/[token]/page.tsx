343691cf7fe73fe7e10fd9445de4a3d6c9fa964c
antoine-le-louet <a.lelouet.freelance@gmail.com>
2026-02-13 15:15:51 +0100

    fix: update ReportPage to use dynamic token from params instead of hardcoded value

--- apps/console-web/src/app/report/[token]/page.tsx
+++ apps/console-web/src/app/report/[token]/page.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { Report } from "@/components/report/types";
 import { argosApi } from "@/lib/ArgosApi";
 import ReportPage from "@/app/report/[token]/ReportPage";
+import { Report } from "@/components/report/types";
 
 export const metadata: Metadata = {
   title: "Rapport Argos",
   robots: { index: false, follow: false },
 };
 
-export default async function ReportPageHome() {
-  const report: Report | null = await argosApi.getReport("nIjDfQeMR9tcQuC_R7MsjhZ6blKezECXfLI5erUKOMM");
+export default async function ReportPageHome({ params }: { params: { token: string } }) {
+  const report: Report = await argosApi.getReport(params.token);
   if (!report) notFound();
 
   return (
